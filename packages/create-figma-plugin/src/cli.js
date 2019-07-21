bf5e9efc152aa540667ee43b76f70f8c4399959c
yuanqing <desk@yuanqing.sg>
2019-07-21 15:59:55 +0800

    s/yargs/sade

--- packages/create-figma-plugin/src/cli.js
+++ packages/create-figma-plugin/src/cli.js
@@ -1,12 +1,19 @@
 #!/usr/bin/env node
 
-const yargs = require('yargs')
-const build = require('./build')
+const build = require('@create-figma-plugin/build')
+const sade = require('sade')
 
-yargs
-  .scriptName('create-figma-plugin')
-  .command(build)
-  .demandCommand()
-  .help()
-  .version()
-  .parse()
+const cli = sade('create-figma-plugin')
+
+cli.command('build', 'Build the plugin', { default: true })
+  .option('-d, --development', 'Build in development mode', false)
+  .action(async function ({ development }) {
+    await build(development, false)
+  })
+
+cli.command('watch', 'Watch and rebuild on changes')
+  .action(async function () {
+    await build(true, true)
+  })
+
+cli.parse(process.argv)
