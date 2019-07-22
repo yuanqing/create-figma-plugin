308862310f424a6be8e75880a89535188b544168
yuanqing <desk@yuanqing.sg>
2019-07-23 04:21:37 +0800

    Refactor

--- packages/build/src/build-manifest.js
+++ packages/build/src/build-manifest.js
@@ -2,38 +2,30 @@ import { outputFile } from 'fs-extra'
 import { join } from 'path'
 import { constants } from '@create-figma-plugin/common'
 
+const pluginCommandsFilePath = join(
+  constants.buildDirectoryName,
+  constants.pluginCommandsFileName
+)
+const pluginUiFilePath = join(
+  constants.buildDirectoryName,
+  constants.pluginUiFileName
+)
+
 export async function buildManifest (config, hasPluginCommands, hasPluginUi) {
-  const menu = createMenu(config.menu)
   const manifest = {
     name: config.name,
     api: constants.apiVersion
   }
   if (hasPluginCommands) {
-    manifest.main = join(
-      constants.buildDirectoryName,
-      constants.pluginCommandsFileName
-    )
+    manifest.main = pluginCommandsFilePath
   }
   if (hasPluginUi) {
-    manifest.ui = join(constants.buildDirectoryName, constants.pluginUiFileName)
+    manifest.ui = pluginUiFilePath
   }
-  if (menu !== null && menu.length > 1) {
+  const menu = config.menu
+  if (menu.length > 1) {
     manifest.menu = menu
   }
   const string = JSON.stringify(manifest, null, 2) + '\n'
   return outputFile(constants.manifestFilePath, string)
-}
-
-function createMenu (menu) {
-  if (menu == null) {
-    return null
-  }
-  return menu.map(function (item) {
-    if (item === '-') {
-      return {
-        separator: true
-      }
-    }
-    return item
-  })
 }
