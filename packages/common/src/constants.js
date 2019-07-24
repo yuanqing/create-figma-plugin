67f60e5afbf31e2f714790dd21e5b6d7b459e056
yuanqing <desk@yuanqing.sg>
2019-07-25 07:35:07 +0800

    s/code/main

--- packages/common/src/constants.js
+++ packages/common/src/constants.js
@@ -8,7 +8,7 @@ export const constants = {
 
   buildDirectoryName: 'build',
   manifestFilePath: 'manifest.json',
-  pluginCodeFileName: 'code.js',
+  pluginCodeFileName: 'main.js',
   pluginUiFileName: 'ui.js',
 
   webpackConfigMutatorFileName: 'create-figma-plugin.js'
