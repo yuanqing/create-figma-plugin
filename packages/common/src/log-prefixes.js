2647e24b11e648ea69df01247f6d44ee5baeb2f3
yuanqing <desk@yuanqing.sg>
2019-07-26 18:47:07 +0800

    s/yellowBright/blue

--- packages/common/src/log-prefixes.js
+++ packages/common/src/log-prefixes.js
@@ -1,7 +1,7 @@
 import chalk from 'chalk'
 
 const error = chalk.red('error')
-const info = chalk.yellowBright('info')
+const info = chalk.blue('info')
 const question = chalk.gray('question')
 const success = chalk.green('success')
 
