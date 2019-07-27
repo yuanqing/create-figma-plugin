e8f6f7c4fd0ebd3d68f6d09eb2b820ad8a741de0
yuanqing <desk@yuanqing.sg>
2019-07-28 01:26:35 +0800

    Fix path

--- packages/create-figma-plugin/src/cli.js
+++ packages/create-figma-plugin/src/cli.js
@@ -1,13 +1,13 @@
 import sade from 'sade'
 import { log } from '@create-figma-plugin/common'
-import { init } from './init'
+import { createFigmaPlugin } from './create-figma-plugin'
 
 sade('create-figma-plugin [name]', true)
   .option('-t, --template', 'Use a template')
   .option('-y, --yes', 'Use defaults', false)
   .action(async function (name, { yes, template }) {
     log.info('Scaffolding a new plugin...')
-    await init({ name, template }, yes)
+    await createFigmaPlugin({ name, template }, yes)
     log.success('Done')
   })
   .parse(process.argv)
