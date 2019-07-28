d8b3b5d17e734d8cf9be2ea86a3977cfbe4ef567
yuanqing <desk@yuanqing.sg>
2019-07-28 17:48:50 +0800

    Fix logging

--- packages/common/src/log.js
+++ packages/common/src/log.js
@@ -1,16 +1,16 @@
 import npmlog from 'npmlog'
 
 function error (message) {
-  npmlog.error(message)
+  npmlog.error('', message)
 }
 
 function info (message) {
-  npmlog.info(message)
+  npmlog.info('', message)
 }
 
-npmlog.addLevel('success', 3001, { fg: 'green', bold: true })
+npmlog.addLevel('success', 3001, { fg: 'green' })
 function success (message) {
-  npmlog.success(message)
+  npmlog.success('', message)
 }
 
 function silenceLogging () {
