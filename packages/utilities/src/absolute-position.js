d1384a3b9978c8c73b6d751ca0e39430bc25d664
yuanqing <desk@yuanqing.sg>
2019-10-19 13:17:56 +0800

    Add `getAbsolutePosition`

--- packages/utilities/src/absolute-position.js
+++ packages/utilities/src/absolute-position.js
@@ -1,3 +1,10 @@
+export function getAbsolutePosition (layer) {
+  return {
+    x: layer.absoluteTransform[0][2],
+    y: layer.absoluteTransform[1][2]
+  }
+}
+
 export const setAbsoluteX = factory('x', 0)
 export const setAbsoluteY = factory('y', 1)
 
