2226382f3dc14a9093a44fb7a2f9f56b9b35d4da
yuanqing <desk@yuanqing.sg>
2019-10-20 14:50:44 +0800

    Exit if layer was already removed

--- packages/utilities/src/traverse-layer.js
+++ packages/utilities/src/traverse-layer.js
@@ -1,6 +1,6 @@
 export function traverseLayer (layer, callback, filterCallback) {
   callback(layer)
-  if (typeof layer.children === 'undefined') {
+  if (layer.removed || typeof layer.children === 'undefined') {
     return
   }
   if (typeof filterCallback === 'function' && filterCallback(layer) === false) {
