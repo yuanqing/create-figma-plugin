3dc3d8dc86574768358512fba8c62fcc4f5797fd
yuanqing <desk@yuanqing.sg>
2019-10-22 15:32:43 +0800

    Move call to `filterCallback`

--- packages/utilities/src/traverse-layer.js
+++ packages/utilities/src/traverse-layer.js
@@ -1,9 +1,12 @@
 export function traverseLayer (layer, callback, filterCallback) {
-  callback(layer)
-  if (layer.removed || typeof layer.children === 'undefined') {
+  if (layer.removed) {
     return
   }
   if (typeof filterCallback === 'function' && filterCallback(layer) === false) {
+    return
+  }
+  callback(layer)
+  if (layer.removed || typeof layer.children === 'undefined') {
     return
   }
   for (const childLayer of layer.children) {
