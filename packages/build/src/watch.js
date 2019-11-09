e3c4d91cf8e378d8fdf004c9dd4f4126eec6298d
yuanqing <desk@yuanqing.sg>
2019-11-09 10:15:15 +0800

    Bikeshed

--- packages/build/src/watch.js
+++ packages/build/src/watch.js
@@ -2,17 +2,16 @@ import chokidar from 'chokidar'
 import { constants, log } from '@create-figma-plugin/common'
 import { build } from './build'
 
-/* eslint-disable */
-// prettier-ignore
-const ignoreRegex = new RegExp([
-  '(^|\\/)', // beginning of string or '/'
-  '\\.',     // '.'
-  '[^.]+',   // one or more characters that isn't '.'
-  `|${constants.build.directoryName}`,
-  `|${constants.build.manifestFilePath}`,
-  '|node_modules'
-].join(''))
-/* eslint-enable */
+const ignoreRegex = new RegExp(
+  [
+    '(^|\\/)', // beginning of string or '/'
+    '\\.', // '.'
+    '[^.]+', // one or more characters that isn't '.'
+    `|${constants.build.directoryName}`,
+    `|${constants.build.manifestFilePath}`,
+    '|node_modules'
+  ].join('')
+)
 
 export function watch () {
   const watcher = chokidar.watch('.', {
