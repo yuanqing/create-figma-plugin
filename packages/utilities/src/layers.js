44b725b526bf1e8c00f917ba4334b16529bb0bf7
yuanqing <desk@yuanqing.sg>
2019-11-09 11:19:27 +0800

    Rename

--- packages/utilities/src/layers.js
+++ packages/utilities/src/layers.js
@@ -1,6 +1,6 @@
 /* global figma */
 
-export function getAllOrSelectedLayers () {
+export function getSelectedLayersOrAllLayers () {
   const selection = figma.currentPage.selection
   return [].concat(
     selection.length === 0 ? figma.currentPage.children : selection
