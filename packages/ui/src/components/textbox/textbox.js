dd928983c8d124fb19564154e34de56e93c621b5
Yuan Qing Lim <yuanqing.lim@shopify.com>
2019-11-15 19:33:12 +0800

    Refactor `Textbox` component

--- packages/ui/src/components/textbox/textbox.js
+++ packages/ui/src/components/textbox/textbox.js
@@ -14,55 +14,63 @@ export function Textbox ({
   focused,
   icon,
   noBorder,
+  numeric,
   onChange,
   value,
   ...rest
 }) {
-  rest.ref = useRef(null)
+  const withIcon = typeof icon !== 'undefined'
+
+  const inputElement = useRef(null)
+
   function handleFocus () {
-    const input = rest.ref.current
-    input.focus()
-    input.select()
+    inputElement.current.focus()
+    inputElement.current.select()
   }
+
   function handleKeyDown (event) {
     if (event.keyCode === UP_KEY_CODE || event.keyCode === DOWN_KEY_CODE) {
-      const value = event.target.value
-      if (nonNumericCharactersRegex.test(value) === true) {
+      event.preventDefault()
+      const value = inputElement.current.value
+      const isNonNumeric = nonNumericCharactersRegex.test(value) === true
+      if (isNonNumeric === true) {
         // Exit if text box contains non-numeric characters
         return
       }
-      const significantFiguresCount = countSignificantFigures(value)
       const number = parseFloat(value)
-      event.preventDefault()
-      const input = rest.ref.current
       const delta = event.shiftKey === true ? 10 : 1
-      input.value = formatValue(
+      const significantFiguresCount = countSignificantFigures(value)
+      inputElement.current.value = formatValue(
         event.keyCode === UP_KEY_CODE ? number + delta : number - delta,
         significantFiguresCount
       )
       onChange(event)
-      input.focus()
-      input.select()
+      handleFocus()
     }
   }
+
   if (focused === true) {
     useEffect(handleFocus, [])
   }
-  const withIcon = typeof icon !== 'undefined'
+
   return (
-    <div class={styles.textbox}>
+    <div
+      class={classnames(
+        styles.textbox,
+        noBorder === true ? styles.noBorder : null,
+        withIcon === true ? styles.withIcon : null
+      )}
+    >
       <input
         {...rest}
-        class={classnames(
-          styles.input,
-          noBorder ? styles.noBorder : null,
-          withIcon ? styles.withIcon : null
-        )}
+        ref={inputElement}
+        type='text'
+        class={styles.input}
         value={value}
         disabled={disabled}
         onChange={onChange}
         onFocus={handleFocus}
-        onKeyDown={handleKeyDown}
+        onKeyDown={numeric === true ? handleKeyDown : null}
       />
       {withIcon ? <div class={styles.icon}>{icon}</div> : null}
     </div>
--- packages/ui/src/components/textbox/textbox.stories.js
+++ packages/ui/src/components/textbox/textbox.stories.js
@@ -4,14 +4,19 @@ import { Textbox } from './textbox'
 
 export default { title: 'Textbox' }
 
+const icon = (
+  <svg viewBox='0 0 8 8' xmlns='http://www.w3.org/2000/svg'>
+    <path
+      d='M0 0v8h8V7H5c0-2.21-1.79-4-4-4V0H0zm1 4v3h3c0-1.657-1.343-3-3-3z'
+      fill-rule='evenodd'
+    />
+  </svg>
+)
+
 function noop () {}
 
-export function Normal () {
+export function Text () {
   return <Textbox value='Text' />
-}
-
-export function Numeric () {
-  return <Textbox value={42} onChange={noop} />
 }
 
 export function WithPlaceholder () {
@@ -27,18 +32,9 @@ export function Disabled () {
 }
 
 export function WithIcon () {
-  return (
-    <Textbox
-      value='Text'
-      onChange={noop}
-      icon={
-        <svg viewBox='0 0 8 8' xmlns='http://www.w3.org/2000/svg'>
-          <path
-            d='M0 0v8h8V7H5c0-2.21-1.79-4-4-4V0H0zm1 4v3h3c0-1.657-1.343-3-3-3z'
-            fill-rule='evenodd'
-          />
-        </svg>
-      }
-    />
-  )
+  return <Textbox value='Text' onChange={noop} icon={icon} />
+}
+
+export function Numeric () {
+  return <Textbox numeric value={42} onChange={noop} />
 }
