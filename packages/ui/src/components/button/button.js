fe337ebbd026478aec7350231f22624c21e1bc55
yuanqing <desk@yuanqing.sg>
2019-11-16 00:20:05 +0800

    Style

--- packages/ui/src/components/button/button.js
+++ packages/ui/src/components/button/button.js
@@ -1,12 +1,17 @@
 /** @jsx h */
 import { h } from 'preact'
 import styles from './button.scss'
+import classnames from '@sindresorhus/class-names'
 
-export function Button ({ secondary, text, ...rest }) {
+export function Button ({ destructive, secondary, text, ...rest }) {
   return (
     <button
       {...rest}
-      class={styles[secondary === true ? 'secondary' : 'button']}
+      class={classnames(
+        styles.button,
+        secondary === true ? styles.secondary : null,
+        destructive === true ? styles.destructive : null
+      )}
     >
       {text}
     </button>
