21815fb02e9635afe667fd4726dda670678fb779
Yuan Qing Lim <yuanqing.lim@shopify.com>
2019-11-15 19:47:51 +0800

    Add paragraph formatting

--- packages/ui/src/components/text/text.stories.js
+++ packages/ui/src/components/text/text.stories.js
@@ -8,6 +8,32 @@ export function Normal () {
   return <Text>Text</Text>
 }
 
+export function Bold () {
+  return (
+    <Text>
+      <strong>Text</strong>
+    </Text>
+  )
+}
+
 export function Muted () {
   return <Text muted>Text</Text>
+}
+
+export function MutedBold () {
+  return (
+    <Text muted>
+      <strong>Text</strong>
+    </Text>
+  )
+}
+
+export function Paragraph () {
+  return (
+    <Text>
+      <p>foo</p>
+      <p>bar</p>
+      <p>baz</p>
+    </Text>
+  )
 }
