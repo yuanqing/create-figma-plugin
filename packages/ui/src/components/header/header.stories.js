c1b31a45c1ed872d6915d3035d6253d460bba2fe
yuanqing <desk@yuanqing.sg>
2019-12-02 21:03:52 +0800

    Delete `header` + add `extraLarge`

--- packages/ui/src/components/header/header.stories.js
+++
@@ -1,9 +0,0 @@
-/** @jsx h */
-import { h } from 'preact'
-import { Header } from './header'
-
-export default { title: 'Header' }
-
-export function Normal () {
-  return <Header>Header</Header>
-}
