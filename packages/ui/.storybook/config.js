bef3df09282b8282715fd2c19e5f37bbf9f2017f
yuanqing <desk@yuanqing.sg>
2019-12-15 15:14:03 +0800

    Add config to make hooks work

--- packages/ui/.storybook/config.js
+++ packages/ui/.storybook/config.js
@@ -1,3 +1,8 @@
-import { configure } from '@storybook/preact'
+/** @jsx h */
+import { h } from 'preact'
+import { addDecorator, configure } from '@storybook/preact'
 
+addDecorator(function (Story) {
+  return <Story />
+})
 configure(require.context('../src', true, /\.stories\.js$/), module)
