1944fcdaf6f8589ea253ca4fb04946bfabde28de
yuanqing <desk@yuanqing.sg>
2019-12-20 01:14:49 +0800

    Add `align`

--- packages/ui/src/components/text/text.js
+++ packages/ui/src/components/text/text.js
@@ -4,12 +4,19 @@ import { h } from 'preact'
 import '../../scss/base.scss'
 import styles from './text.scss'
 
-export function Text ({ bold: isBold, muted: isMuted, ...rest }) {
+export function Text ({
+  align = 'left',
+  bold: isBold,
+  muted: isMuted,
+  ...rest
+}) {
   return (
     <div
       {...rest}
       class={classnames(
         styles.text,
+        styles[align],
+        isBold === true ? styles.isBold : null,
         isBold === true ? styles.isBold : null,
         isMuted === true ? styles.isMuted : null
       )}
