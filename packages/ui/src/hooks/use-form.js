e6a9c222502be6465ba05bd155adaf3809608dbc
yuanqing <desk@yuanqing.sg>
2019-12-25 22:58:11 +0800

    Add validation

--- packages/ui/src/hooks/use-form.js
+++ packages/ui/src/hooks/use-form.js
@@ -1,21 +1,18 @@
 import { useCallback, useEffect, useState } from 'preact/hooks'
 import { ENTER_KEY_CODE, ESCAPE_KEY_CODE } from '../utilities/key-codes'
 
-export function useForm (
-  initialInputs,
-  handleSubmitCallback,
-  handleCancelCallback,
-  shouldHandleKeyDown
-) {
+export function useForm (initialInputs, { close, submit, validate }) {
   const [inputs, setInputs] = useState(initialInputs)
   const handleSubmit = useCallback(
     function (event) {
       if (typeof event !== 'undefined') {
         event.preventDefault()
       }
-      handleSubmitCallback(inputs)
+      if (typeof validate !== 'function' || validate(inputs) === true) {
+        submit(inputs)
+      }
     },
-    [inputs, handleSubmitCallback]
+    [inputs, submit, validate]
   )
   function handleInput (value, name) {
     setInputs(function (inputs) {
@@ -27,31 +24,41 @@ export function useForm (
   }
   const handleKeyDown = useCallback(
     function (event) {
-      if (event.keyCode === ENTER_KEY_CODE) {
-        handleSubmitCallback(inputs)
+      if (
+        event.keyCode === ENTER_KEY_CODE &&
+        (typeof validate !== 'function' || validate(inputs) === true)
+      ) {
+        submit(inputs)
         return
       }
       if (event.keyCode === ESCAPE_KEY_CODE) {
-        handleCancelCallback()
+        close()
       }
     },
-    [handleCancelCallback, handleSubmitCallback, inputs]
+    [close, inputs, submit, validate]
+  )
+  const isValid = useCallback(
+    function () {
+      if (typeof validate !== 'function') {
+        throw new Error('Need a `validate` callback')
+      }
+      return validate(inputs)
+    },
+    [inputs, validate]
   )
   useEffect(
     function () {
-      if (shouldHandleKeyDown === true) {
-        window.addEventListener('keydown', handleKeyDown)
-        return function () {
-          window.removeEventListener('keydown', handleKeyDown)
-        }
+      window.addEventListener('keydown', handleKeyDown)
+      return function () {
+        window.removeEventListener('keydown', handleKeyDown)
       }
     },
-    [handleKeyDown, shouldHandleKeyDown]
+    [handleKeyDown]
   )
   return {
     inputs,
-    setInputs,
     handleSubmit,
-    handleInput
+    handleInput,
+    isValid
   }
 }
