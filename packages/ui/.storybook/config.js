9841a84fa04bed7dfb282d703b6313e4df7e19f6
yuanqing <desk@yuanqing.sg>
2019-12-27 10:09:36 +0800

    Sort stories alphabetically

--- packages/ui/.storybook/config.js
+++ packages/ui/.storybook/config.js
@@ -1,8 +1,19 @@
 /** @jsx h */
 import { h } from 'preact'
-import { addDecorator, configure } from '@storybook/preact'
+import { addDecorator, addParameters, configure } from '@storybook/preact'
 
 addDecorator(function (Story) {
   return <Story />
 })
+
+addParameters({
+  options: {
+    storySort: function (a, b) {
+      return a[1].kind === b[1].kind
+        ? 0
+        : a[1].id.localeCompare(b[1].id, undefined, { numeric: true })
+    }
+  }
+})
+
 configure(require.context('../src', true, /\.stories\.js$/), module)
