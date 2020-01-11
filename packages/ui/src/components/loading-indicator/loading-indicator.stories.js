cdc0250b89c088f436d70126b19fad94463dce98
yuanqing <desk@yuanqing.sg>
2020-01-11 23:54:13 +0800

    Add Loading Indicator

---
+++ packages/ui/src/components/loading-indicator/loading-indicator.stories.js
@@ -0,0 +1,9 @@
+/** @jsx h */
+import { h } from 'preact'
+import { LoadingIndicator } from './loading-indicator'
+
+export default { title: 'Loading Indicator' }
+
+export const Default = function () {
+  return <LoadingIndicator />
+}
