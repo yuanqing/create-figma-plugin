cecb1f55ebd018e0b00e5a3c3dee791a2c44be01
yuanqing <desk@yuanqing.sg>
2020-01-27 22:21:23 +0800

    Refactor + extract comparator

--- packages/utilities/src/layers/sort-layers/sort-layers-by-name.js
+++ packages/utilities/src/layers/sort-layers/sort-layers-by-name.js
@@ -1,20 +1,22 @@
 export function sortLayersByName (layers) {
-  return [].concat(layers).sort(function (a, b) {
-    const aName = a.name.toLowerCase()
-    const bName = b.name.toLowerCase()
-    if (aName === bName) {
-      if (a.type !== 'PAGE') {
-        const yPositionDifference = a.y - b.y
-        if (yPositionDifference !== 0) {
-          return yPositionDifference
-        }
-        const xPositionDifference = a.x - b.x
-        if (xPositionDifference !== 0) {
-          return xPositionDifference
-        }
-      }
-      return a.id.localeCompare(b.id, { numeric: true })
-    }
+  return layers.slice().sort(sortComparator)
+}
+
+function sortComparator (a, b) {
+  const aName = a.name.toLowerCase()
+  const bName = b.name.toLowerCase()
+  if (aName !== bName) {
     return aName.localeCompare(bName, { numeric: true })
-  })
+  }
+  if (a.type !== 'PAGE') {
+    const yPositionDifference = a.y - b.y
+    if (yPositionDifference !== 0) {
+      return yPositionDifference
+    }
+    const xPositionDifference = a.x - b.x
+    if (xPositionDifference !== 0) {
+      return xPositionDifference
+    }
+  }
+  return a.id.localeCompare(b.id, { numeric: true })
 }
