089b1c1df68f979a4effc397a891ab3ab69aedd2
yuanqing <desk@yuanqing.sg>
2020-01-27 22:20:23 +0800

    Sort sibling layers based on their index

--- packages/utilities/src/layers/group-sibling-layers.js
+++ packages/utilities/src/layers/group-sibling-layers.js
@@ -1,12 +1,29 @@
 export function groupSiblingLayers (layers) {
+  const groups = resolveGroups(layers)
+  const result = []
+  for (const group of groups) {
+    const layers = group.sort(sortComparator).map(function ({ layer }) {
+      return layer
+    })
+    result.push(layers)
+  }
+  return result
+}
+
+function resolveGroups (layers) {
   const result = {}
   for (const layer of layers) {
     const parentId = layer.parent.id
+    const index = layer.parent.children.indexOf(layer) // so that we can sort layers by their `index`
     if (typeof result[parentId] === 'undefined') {
-      result[parentId] = [layer]
+      result[parentId] = [{ layer, index }]
     } else {
-      result[parentId].push(layer)
+      result[parentId].push({ layer, index })
     }
   }
   return Object.values(result)
+}
+
+function sortComparator (a, b) {
+  return b.index - a.index
 }
