96d984504a24ac7708c9587b804c88e8f6be7ed6
yuanqing <desk@yuanqing.sg>
2020-02-13 21:31:49 +0800

    Remove unnecessary `undefined`

--- packages/ui/.storybook/config.js
+++ packages/ui/.storybook/config.js
@@ -11,7 +11,7 @@ addParameters({
     storySort: function (a, b) {
       return a[1].kind === b[1].kind
         ? 0
-        : a[1].id.localeCompare(b[1].id, undefined, { numeric: true })
+        : a[1].id.localeCompare(b[1].id, { numeric: true })
     }
   }
 })
