5c4112a6d1f5987ecbd692463aecd1d9f3be0906
yuanqing <desk@yuanqing.sg>
2020-04-05 15:32:13 +0800

    Refactor

--- packages/utilities/src/ui.js
+++ packages/utilities/src/ui.js
@@ -1,20 +1,20 @@
 /* global __html__ */
 
 const CLOSING_SLASH = '/'
-export function showUI (options, data = {}) {
+
+export function showUI ({ width, height, initialState = {} } = {}) {
   if (typeof __html__ === 'undefined') {
     throw new Error(
       `UI not defined for the command corresponding to \`${global.__command__}\``
     )
   }
   const html = `
-    <style>body { margin: 0; }</style>
     <div id="create-figma-plugin"></div>
     <script>
-      window.__data__ = ${JSON.stringify(data)};
       window.__command__ = '${global.__command__}';
+      window.__initialState__ = ${JSON.stringify(initialState)};
       ${__html__}
     <${CLOSING_SLASH}script>
   `
-  figma.showUI(html, options)
+  figma.showUI(html, { width, height })
 }
