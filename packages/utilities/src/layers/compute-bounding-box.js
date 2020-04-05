b5f9c4a12a3a8c922ab6e5aba2b8d89040d7b2f5
yuanqing <desk@yuanqing.sg>
2020-04-05 15:29:59 +0800

    Fix calculation of nested layers

--- packages/utilities/src/layers/compute-bounding-box.js
+++ packages/utilities/src/layers/compute-bounding-box.js
@@ -1,12 +1,16 @@
+import { getAbsolutePosition } from './absolute-position/get-absolute-position'
+
 export function computeBoundingBox (layer) {
   if (layer.rotation === 0) {
-    const { x, y, width, height } = layer
-    return { x, y, width, height }
+    const absolutePosition = getAbsolutePosition(layer)
+    const { width, height } = layer
+    return { ...absolutePosition, width, height }
   }
   const parent = layer.parent
   const index = parent.children.indexOf(layer)
   const group = figma.group([layer], parent, index)
-  const { x, y, width, height } = group
-  parent.insertChild(index, layer)
-  return { x, y, width, height }
+  const absolutePosition = getAbsolutePosition(group)
+  const { width, height } = group
+  parent.insertChild(index, layer) // ungroup the layer
+  return { ...absolutePosition, width, height }
 }
