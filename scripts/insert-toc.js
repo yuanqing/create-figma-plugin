6f12aa62e7b0d552db0bd218a362bfb9c63cdea3
yuanqing <desk@yuanqing.sg>
2020-04-07 23:34:59 +0800

    Fix regex and file path

--- scripts/insert-toc.js
+++ scripts/insert-toc.js
@@ -3,11 +3,11 @@ const fs = require('fs-extra')
 const path = require('path')
 
 async function main () {
-  const filename = path.resolve(__dirname, '..', 'docs', '3-api.md')
+  const filename = path.resolve(__dirname, '..', 'docs', 'api.md')
   const contents = await fs.readFile(filename, 'utf8')
   const result = markdownToc
     .insert(contents, { maxdepth: 3, bullet: '-' })
-    .replace(/\[const \w+ = /g, '[') // remove `const = `
+    .replace(/\[const [\w\s,{}[\]]+ = /g, '[') // remove `const = `
     .replace(/- \[([^\]]+)\]/g, '- [**$1**]') // make headers bold
   await fs.writeFile(filename, result, 'utf8')
 }
