b23b98b7af1df1b77f2866e22baff8609259656c
yuanqing <desk@yuanqing.sg>
2020-04-12 22:23:42 +0800

    Only load each font once

--- packages/utilities/src/layers/load-fonts-async.js
+++ packages/utilities/src/layers/load-fonts-async.js
@@ -1,13 +1,28 @@
 export function loadFontsAsync (layers) {
-  const promises = []
+  let fonts = []
   for (const layer of layers) {
     if (layer.type !== 'TEXT') {
       continue
     }
-    const length = layer.characters.length
-    for (let i = 0; i < length; i++) {
-      promises.push(figma.loadFontAsync(layer.getRangeFontName(i, i + 1)))
+    fonts = fonts.concat(computeUniqueFontsUsedInLayer(layer))
+  }
+  return Promise.all(
+    fonts.map(function (font) {
+      return figma.loadFontAsync(font)
+    })
+  )
+}
+
+function computeUniqueFontsUsedInLayer (textLayer) {
+  const result = {}
+  let i = -1
+  const length = textLayer.characters.length
+  while (++i < length) {
+    const fontName = textLayer.getRangeFontName(i, i + 1)
+    const key = `${fontName.family}-${fontName.style}`
+    if (typeof result[key] === 'undefined') {
+      result[key] = fontName
     }
   }
-  return Promise.all(promises)
+  return Object.values(result)
 }
