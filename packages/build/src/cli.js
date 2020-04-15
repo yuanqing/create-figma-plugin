a230f4aca587ea7e9dc8117c1f7228751a02a514
yuanqing <desk@yuanqing.sg>
2020-04-15 19:49:09 +0800

    s/create-figma-plugin-build/build-figma-plugin

--- packages/build/src/cli.js
+++ packages/build/src/cli.js
@@ -4,7 +4,7 @@ import sade from 'sade'
 import { buildAsync } from './build-async'
 import { watch } from './watch'
 
-sade('create-figma-plugin-build', true)
+sade('build-figma-plugin', true)
   .option('-d, --dev', 'Build in development mode', false)
   .option('-w, --watch', 'Rebuild the plugin on changes', false)
   .action(async function ({ dev: isDevelopment, watch: isWatch }) {
