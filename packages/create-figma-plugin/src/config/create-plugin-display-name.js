af3f5cc019a54b21208894b7367a3692ed2f9381
yuanqing <desk@yuanqing.sg>
2020-04-19 18:37:45 +0800

    Use `titlecase` instead

--- packages/create-figma-plugin/src/config/create-plugin-display-name.js
+++ packages/create-figma-plugin/src/config/create-plugin-display-name.js
@@ -1,9 +1,9 @@
 import { constants } from '@create-figma-plugin/common'
-import { capitalCase } from 'capital-case'
+import titleCase from 'titlecase'
 
 export function createPluginDisplayName (name) {
   if (typeof name === 'undefined') {
     return constants.packageJson.defaultPluginDisplayName
   }
-  return capitalCase(name.replace(/^figma-/, ''))
+  return titleCase(name.replace(/^figma-/, '').replace(/-/g, ' '))
 }
