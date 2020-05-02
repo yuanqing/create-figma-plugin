b22b045448f6059cf8535181e1d23813c167032a
yuanqing <desk@yuanqing.sg>
2020-05-02 15:45:34 +0800

    Remove `mapNumberToWord`

--- packages/utilities/src/string.js
+++ packages/utilities/src/string.js
@@ -10,25 +10,6 @@ export function formatErrorMessage (message) {
   return `${CROSS} ${SPACE} ${message}`
 }
 
-const numbers = [
-  'zero',
-  'one',
-  'two',
-  'three',
-  'four',
-  'five',
-  'six',
-  'seven',
-  'eight',
-  'nine'
-]
-export function mapNumberToWord (number) {
-  if (number > -1 && number < 10) {
-    return numbers[number]
-  }
-  return `${number}`
-}
-
 export function pluralize (number, singular, plural) {
   if (number === 1) {
     return singular
