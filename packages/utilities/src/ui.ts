0346116b050baf0e2f3f18212649138db7711fe2
yuanqing <desk@yuanqing.sg>
2020-05-17 07:39:47 +0800

    Set __COMMAND__ to null explicitly

--- packages/utilities/src/ui.ts
+++ packages/utilities/src/ui.ts
@@ -9,7 +9,9 @@ export function showUI (options: ShowUIOptions, data: any = {}): void {
   const html = `
     <div id="create-figma-plugin"></div>
     <script>
-      const __COMMAND__ = '${figma.command}';
+      const __COMMAND__ = ${
+        figma.command === '' ? 'null' : `'${figma.command}'`
+      };
       const __DATA__ = ${JSON.stringify(data)};
       ${__html__}
     <${CLOSING_SLASH}script>
