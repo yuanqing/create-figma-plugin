bb28e8c2501cf7dc342dff2dc242560435d987eb
yuanqing <desk@yuanqing.sg>
2020-05-17 00:05:46 +0800

    Remove props

--- packages/ui/src/render.tsx
+++ packages/ui/src/render.tsx
@@ -2,8 +2,8 @@
 import { h, render as preactRender } from 'preact'
 require('./scss/base.scss')
 
-export function render (Plugin, props) {
+export function render (Plugin) {
   return function (rootNode, data) {
-    preactRender(<Plugin {...data} {...props} />, rootNode)
+    preactRender(<Plugin {...data} />, rootNode)
   }
 }
