ccdce4469799d1022406a772a097825f3124b066
yuanqing <desk@yuanqing.sg>
2020-05-17 23:04:37 +0800

    Revert `prop`

--- packages/ui/src/render.tsx
+++ packages/ui/src/render.tsx
@@ -2,8 +2,11 @@
 import { h, render as preactRender } from 'preact'
 require('./scss/base.scss')
 
-export function render (Plugin: preact.FunctionComponent<any>) {
-  return function (rootNode: HTMLElement, data: { [key: string]: any }) : void {
-    preactRender(<Plugin {...data} />, rootNode)
+export function render (
+  Plugin: preact.FunctionComponent<any>,
+  props?: { [key: string]: any }
+) {
+  return function (rootNode: HTMLElement, data: { [key: string]: any }): void {
+    preactRender(<Plugin {...props} {...data} />, rootNode)
   }
 }
