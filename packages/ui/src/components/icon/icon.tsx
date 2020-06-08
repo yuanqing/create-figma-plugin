30030c49e1262d92124d224a07b0f1e9828c8ac0
yuanqing <desk@yuanqing.sg>
2020-06-08 08:01:11 +0800

    Use kebab-case props

--- packages/ui/src/components/icon/icon.tsx
+++ packages/ui/src/components/icon/icon.tsx
@@ -8,9 +8,11 @@ export interface IconProps {
 }
 
 export function Icon({ children }: IconProps): h.JSX.Element {
+  /* eslint-disable react/no-unknown-property */
   return (
-    <svg className={styles.icon}>
-      <path fillRule="evenodd" clipRule="evenodd" d={children} />
+    <svg className={styles.icon} xmlns="http://www.w3.org/2000/svg">
+      <path fill-rule="evenodd" clip-rule="evenodd" d={children} />
     </svg>
   )
+  /* eslint-enable react/no-unknown-property */
 }
