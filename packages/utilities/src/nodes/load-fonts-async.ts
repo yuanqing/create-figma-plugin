e3d691ed6f9570b75f013ec04de4b8d75946e11e
yuanqing <desk@yuanqing.sg>
2020-08-28 20:18:00 +0800

    Refactor `loadFontsAsync`

--- packages/utilities/src/nodes/load-fonts-async.ts
+++ packages/utilities/src/nodes/load-fonts-async.ts
@@ -2,33 +2,44 @@
  * Loads the fonts used in all the text nodes in `nodes`.
  */
 export async function loadFontsAsync(nodes: Array<SceneNode>): Promise<void> {
-  let fonts: Array<FontName> = []
+  const result: { [key: string]: FontName } = {}
   for (const node of nodes) {
     if (node.type === 'TEXT') {
-      fonts = fonts.concat(computeUniqueFontsUsedInNode(node))
+      collectFontsUsedInNode(node, result)
     }
   }
   await Promise.all(
-    fonts.map(function (font) {
+    Object.values(result).map(function (font) {
       return figma.loadFontAsync(font)
     })
   )
 }
 
-function computeUniqueFontsUsedInNode(node: TextNode): Array<FontName> {
+function collectFontsUsedInNode(
+  node: TextNode,
+  result: { [key: string]: FontName }
+): void {
   const length = node.characters.length
   if (length === 0) {
-    return [node.fontName as FontName]
+    const fontName = node.fontName as FontName
+    const key = createKey(fontName)
+    if (key in result) {
+      return
+    }
+    result[key] = fontName
+    return
   }
-  const result: { [key: string]: FontName } = {}
   let i = -1
   while (++i < length) {
     const fontName = node.getRangeFontName(i, i + 1) as FontName
-    const key = `${fontName.family}-${fontName.style}`
+    const key = createKey(fontName)
     if (key in result) {
       continue
     }
     result[key] = fontName
   }
-  return Object.values(result)
+}
+
+function createKey(fontName: FontName) {
+  return `${fontName.family}-${fontName.style}`
 }
