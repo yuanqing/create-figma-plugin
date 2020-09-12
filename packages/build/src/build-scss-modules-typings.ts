3f70ae54c119a42e4d5988a9b73750423fe030fe
yuanqing <desk@yuanqing.sg>
2020-09-12 10:16:35 +0800

    Exit if no CSS/SCSS files

--- packages/build/src/build-scss-modules-typings.ts
+++ packages/build/src/build-scss-modules-typings.ts
@@ -1,7 +1,12 @@
 import { constants } from '@create-figma-plugin/common'
 import * as execa from 'execa'
+import * as globby from 'globby'
 
 export async function buildScssModulesTypings(): Promise<void> {
   const pattern = `${constants.src.directory}/**/*.{css,scss}`
+  const filePaths = await globby(pattern)
+  if (filePaths.length === 0) {
+    return
+  }
   await execa('./node_modules/.bin/tsm', [pattern, '--implementation', 'sass'])
 }
