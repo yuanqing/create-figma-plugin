31401bb436b5d9d15607fc1cac2884e43c18cc8b
yuanqing <desk@yuanqing.sg>
2020-09-20 15:47:33 +0800

    Add `symlink.sh` script to allow testing locally

---
+++ scripts/symlink.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+set -e
+
+main() {
+  if [ "$#" -eq 0 ]; then
+    echo "Need a source directory" >&2
+    return
+  fi
+  SOURCE_DIRECTORY=$(cd "$1"; pwd)
+
+  if [ "$#" -eq 1 ]; then
+    echo "Need a target directory" >&2
+    return
+  fi
+  TARGET_DIRECTORY=$(cd "$2"; pwd)
+
+  rm -rf "$TARGET_DIRECTORY"/node_modules/@create-figma-plugin
+  mkdir "$TARGET_DIRECTORY"/node_modules/@create-figma-plugin
+  ln -s "$SOURCE_DIRECTORY"/packages/build "$TARGET_DIRECTORY"/node_modules/@create-figma-plugin
+  ln -s "$SOURCE_DIRECTORY"/packages/common "$TARGET_DIRECTORY"/node_modules/@create-figma-plugin
+  ln -s "$SOURCE_DIRECTORY"/packages/tsconfig "$TARGET_DIRECTORY"/node_modules/@create-figma-plugin
+  ln -s "$SOURCE_DIRECTORY"/packages/ui "$TARGET_DIRECTORY"/node_modules/@create-figma-plugin
+  ln -s "$SOURCE_DIRECTORY"/packages/utilities "$TARGET_DIRECTORY"/node_modules/@create-figma-plugin
+
+  rm -rf "$TARGET_DIRECTORY"/node_modules/.bin/build-figma-plugin
+  ln -s "$SOURCE_DIRECTORY"/packages/build/lib/cli.js "$TARGET_DIRECTORY"/node_modules/.bin/build-figma-plugin
+  chmod +x "$TARGET_DIRECTORY"/node_modules/.bin/build-figma-plugin
+}
+
+main "$@"
