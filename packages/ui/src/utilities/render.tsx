440b2ced96d5bc43116e315ab353a77f1ef44330
yuanqing <desk@yuanqing.sg>
2021-04-21 08:57:20 +0800

    Rename

--- packages/ui/src/utilities/render.tsx
+++ packages/ui/src/utilities/render.tsx
@@ -5,7 +5,7 @@ import type { FunctionComponent } from 'preact'
 import { h, render as preactRender } from 'preact'
 
 export function render<Props>(Plugin: FunctionComponent<Props>) {
-  return function (rootNode: HTMLElement, data: Props) {
-    preactRender(<Plugin {...data} />, rootNode)
+  return function (rootNode: HTMLElement, props: Props) {
+    preactRender(<Plugin {...props} />, rootNode)
   }
 }
