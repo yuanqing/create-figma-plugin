83b840e0408e4b93e7bf0fac4ebc51d377cf150a
yuanqing <desk@yuanqing.sg>
2021-04-26 18:56:52 +0800

    Remove props

--- packages/ui/src/components/icon/icon-frame/icon-frame.ts
+++ packages/ui/src/components/icon/icon-frame/icon-frame.ts
@@ -1,7 +1,5 @@
 import { createIcon } from '../create-icon'
 
-export type { IconProps as IconFrameProps } from '../create-icon'
-
 export const IconFrame = createIcon(
   'M6 2.5H5V5H2.5v1H5v4H2.5v1H5v2.5h1V11h4v2.5h1V11h2.5v-1H11V6h2.5V5H11V2.5h-1V5H6V2.5zM6 6v4h4V6H6z'
 )
--- packages/ui/src/components/icon/icon-move-right/icon-move-right.ts
+++ packages/ui/src/components/icon/icon-move-right/icon-move-right.ts
@@ -1,7 +1,5 @@
 import { createIcon } from '../create-icon'
 
-export type { IconProps as IconMoveRightProps } from '../create-icon'
-
 export const IconMoveRight = createIcon(
   'M3 8.5V12H4L4 8.5H11.7929L9.64645 10.6464L10.3536 11.3536L13.3536 8.35355L13.7071 8L13.3536 7.64645L10.3536 4.64645L9.64645 5.35355L11.7929 7.5H4L4 4H3V7.5V8.5Z'
 )
