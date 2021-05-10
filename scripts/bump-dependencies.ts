6ff5e1fda652350e84c7cb09a68686f3284ac04e
yuanqing <desk@yuanqing.sg>
2021-05-11 00:59:12 +0800

    Refactor

--- scripts/bump-dependencies.ts
+++ scripts/bump-dependencies.ts
@@ -1,22 +1,26 @@
 import globby from 'globby'
-import ncu from 'npm-check-updates'
+import ncu, { RunResults } from 'npm-check-updates'
 import { dirname, join, resolve } from 'path'
 import { fileURLToPath } from 'url'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 async function main(): Promise<void> {
-  const args = process.argv.slice(2) // modules passed in as CLI arguments will _not_ be bumped
+  const args = process.argv.slice(2) // Modules passed in as CLI arguments will _not_ be bumped
   const parentDirectoryPath = resolve(__dirname, '..')
-  const globs = [
+  const packageJsonFiles = [
     join(parentDirectoryPath, 'package.json'),
-    join(parentDirectoryPath, 'packages', '**', 'package.json')
+    ...(await globby(
+      join(parentDirectoryPath, 'packages', '**', 'package.json'),
+      {
+        deep: 2
+      }
+    ))
   ]
-  const packageJsonFiles = await globby(globs, { deep: 2 })
-  const promises: Array<Promise<void>> = []
-  packageJsonFiles.forEach(function (packageJsonFile: string): void {
-    async function runAsync(): Promise<void> {
-      await ncu.run({
+  const promises: Array<Promise<RunResults>> = []
+  for (const packageJsonFile of packageJsonFiles) {
+    promises.push(
+      ncu.run({
         packageFile: packageJsonFile,
         packageManager: 'npm',
         reject: args,
@@ -24,9 +28,8 @@ async function main(): Promise<void> {
         target: 'latest',
         upgrade: true
       })
-    }
-    promises.push(runAsync())
-  })
+    )
+  }
   await Promise.all(promises)
 }
 main()
