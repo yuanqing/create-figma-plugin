5f92fd3e72ccade39011731ff0bb8876a8577279
yuanqing <desk@yuanqing.sg>
2021-05-15 17:57:20 +0800

    Remove the `Name` type parameter

--- packages/ui/src/types.ts
+++ packages/ui/src/types.ts
@@ -1,9 +1,6 @@
 import { JSX, RenderableProps } from 'preact'
 
-export type OnValueChange<Value, Name extends string> = (
-  newValue: Value,
-  name: undefined | Name
-) => void
+export type OnValueChange<Value> = (newValue: Value, name?: string) => void
 
 export type Props<
   Target extends EventTarget = EventTarget,
