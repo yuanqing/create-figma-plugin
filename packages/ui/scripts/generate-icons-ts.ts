5eb75d83ca98a983c952cfc28c4af67727d28668
yuanqing <desk@yuanqing.sg>
2021-06-12 14:18:00 +0800

    Prettier

--- packages/ui/scripts/generate-icons-ts.ts
+++ packages/ui/scripts/generate-icons-ts.ts
@@ -41,8 +41,9 @@ async function generateIconsTs(
     throw new Error(`No files match \`${globPatterns.join(', ')}\``)
   }
   const svgFiles = await readSvgFilesAsync(filePaths)
-  const dimensions: Record<string, Array<SvgFile>> =
-    groupSvgFilesByDimension(svgFiles)
+  const dimensions: Record<string, Array<SvgFile>> = groupSvgFilesByDimension(
+    svgFiles
+  )
   const directoryPaths = await globby(join(outputDirectoryPath, 'icon-*'), {
     onlyFiles: false
   })
