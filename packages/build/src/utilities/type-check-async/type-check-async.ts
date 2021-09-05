c15436973b7cc531d04d326656c55deead8dbf05
Yuan Qing Lim <desk@yuanqing.sg>
2021-09-05 15:05:15 +0800

    Edit file paths

--- packages/build/src/utilities/type-check-async/type-check-async.ts
+++ packages/build/src/utilities/type-check-async/type-check-async.ts
@@ -93,8 +93,8 @@ function filterDiagnostics(
 ): Array<ts.Diagnostic> {
   return diagnostics.filter(function (diagnostic: ts.Diagnostic): boolean {
     // The `console` global defined in `@figma/plugin-typings` clashes with
-    // the `console` global defined by the `dom` TypeScript `lib`. We suppress
-    // this specific error when type-checking.
+    // the `console` global defined by the `dom` and `@types/nodes` libraries.
+    // We suppress this specific error when type-checking.
     // See https://github.com/figma/plugin-typings/pull/52/files#diff-7aa4473ede4abd9ec099e87fec67fd57afafaf39e05d493ab4533acc38547eb8
 
     if (diagnostic.code !== 2451 || typeof diagnostic.file === 'undefined') {
@@ -102,10 +102,9 @@ function filterDiagnostics(
     }
     const fileName = diagnostic.file.fileName
     if (
-      fileName.indexOf('/node_modules/typescript/lib/lib.dom.d.ts') === -1 &&
-      fileName.indexOf('/node_modules/@figma/plugin-typings/index.d.ts') ===
-        -1 &&
-      fileName.indexOf('/node_modules/@types/node/globals.d.ts') === -1
+      fileName.indexOf('typescript/lib/lib.dom.d.ts') === -1 &&
+      fileName.indexOf('@figma/plugin-typings/index.d.ts') === -1 &&
+      fileName.indexOf('@types/node/globals.d.ts') === -1
     ) {
       return true
     }
