13cba4fde80766f890815adb69f90392c73a7b53
yuanqing <desk@yuanqing.sg>
2021-09-20 22:54:35 +0200

    Swap out `react-dom` too

--- packages/build/src/utilities/build-bundles-async/esbuild-preact-compat-plugin.ts
+++ packages/build/src/utilities/build-bundles-async/esbuild-preact-compat-plugin.ts
@@ -8,17 +8,20 @@ export function esbuildPreactCompatPlugin(): Plugin {
   return {
     name: 'preact-compat',
     setup: function (build: PluginBuild): void {
-      build.onResolve({ filter: /^react$/ }, async function (): Promise<{
-        path: string
-      }> {
-        const preactPath = await findUp(
-          join('node_modules', 'preact', 'compat', 'dist', 'compat.module.js')
-        )
-        if (typeof preactPath === 'undefined') {
-          throw new Error('Cannot find `preact`')
+      build.onResolve(
+        { filter: /^react(?:-dom)?$/ },
+        async function (): Promise<{
+          path: string
+        }> {
+          const preactCompatPath = await findUp(
+            join('node_modules', 'preact', 'compat', 'dist', 'compat.module.js')
+          )
+          if (typeof preactCompatPath === 'undefined') {
+            throw new Error('Cannot find `preact`')
+          }
+          return { path: preactCompatPath }
         }
-        return { path: preactPath }
-      })
+      )
     }
   }
 }
