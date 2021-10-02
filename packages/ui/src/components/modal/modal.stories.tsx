a52f88b5055382e5d1c7aedfa5d314ff49c7db87
Yuan Qing Lim <desk@yuanqing.sg>
2021-10-02 23:21:42 +0800

    Add `Modal` component

---
+++ packages/ui/src/components/modal/modal.stories.tsx
@@ -0,0 +1,236 @@
+/** @jsx h */
+import { Fragment, h } from 'preact'
+import { useState } from 'preact/hooks'
+
+import { Button } from '../button/button'
+import { IconReturn32 } from '../icon/icon-32/icon-return-32'
+import { Modal } from './modal'
+
+export default { title: 'Components/Modal' }
+
+export const Default = function () {
+  const [isOpen, setIsOpen] = useState(false)
+  function handleOpenButtonClick() {
+    setIsOpen(true)
+  }
+  function handleCloseButtonClick() {
+    setIsOpen(false)
+  }
+  const style = { height: '160px', padding: '12px', width: '240px' }
+  return (
+    <Fragment>
+      <Button fullWidth onClick={handleOpenButtonClick}>
+        Open
+      </Button>
+      <Modal isOpen={isOpen}>
+        <div style={style}>
+          <Button onClick={handleCloseButtonClick} secondary>
+            Close
+          </Button>
+        </div>
+      </Modal>
+    </Fragment>
+  )
+}
+
+export const PositionBottom = function () {
+  const [isOpen, setIsOpen] = useState(false)
+  function handleOpenButtonClick() {
+    setIsOpen(true)
+  }
+  function handleCloseButtonClick() {
+    setIsOpen(false)
+  }
+  const style = { height: '160px', padding: '12px' }
+  return (
+    <Fragment>
+      <Button fullWidth onClick={handleOpenButtonClick}>
+        Open
+      </Button>
+      <Modal isOpen={isOpen} position="bottom">
+        <div style={style}>
+          <Button onClick={handleCloseButtonClick} secondary>
+            Close
+          </Button>
+        </div>
+      </Modal>
+    </Fragment>
+  )
+}
+
+export const PositionLeft = function () {
+  const [isOpen, setIsOpen] = useState(false)
+  function handleOpenButtonClick() {
+    setIsOpen(true)
+  }
+  function handleCloseButtonClick() {
+    setIsOpen(false)
+  }
+  const style = { padding: '12px', width: '240px' }
+  return (
+    <Fragment>
+      <Button fullWidth onClick={handleOpenButtonClick}>
+        Open
+      </Button>
+      <Modal isOpen={isOpen} position="left">
+        <div style={style}>
+          <Button onClick={handleCloseButtonClick} secondary>
+            Close
+          </Button>
+        </div>
+      </Modal>
+    </Fragment>
+  )
+}
+
+export const PositionRight = function () {
+  const [isOpen, setIsOpen] = useState(false)
+  function handleOpenButtonClick() {
+    setIsOpen(true)
+  }
+  function handleCloseButtonClick() {
+    setIsOpen(false)
+  }
+  const style = { padding: '12px', width: '240px' }
+  return (
+    <Fragment>
+      <Button fullWidth onClick={handleOpenButtonClick}>
+        Open
+      </Button>
+      <Modal isOpen={isOpen} position="right">
+        <div style={style}>
+          <Button onClick={handleCloseButtonClick} secondary>
+            Close
+          </Button>
+        </div>
+      </Modal>
+    </Fragment>
+  )
+}
+
+export const CloseOnOverlayClick = function () {
+  const [isOpen, setIsOpen] = useState(false)
+  function handleOpenButtonClick() {
+    setIsOpen(true)
+  }
+  function handleOverlayClick() {
+    setIsOpen(false)
+  }
+  const style = { height: '160px', padding: '12px', width: '240px' }
+  return (
+    <Fragment>
+      <Button fullWidth onClick={handleOpenButtonClick}>
+        Open
+      </Button>
+      <Modal isOpen={isOpen} onOverlayClick={handleOverlayClick}>
+        <div style={style}>foo</div>
+      </Modal>
+    </Fragment>
+  )
+}
+
+export const Title = function () {
+  const [isOpen, setIsOpen] = useState(false)
+  function handleOpenButtonClick() {
+    setIsOpen(true)
+  }
+  function handleCloseButtonClick() {
+    setIsOpen(false)
+  }
+  const style = { height: '160px', padding: '12px', width: '240px' }
+  return (
+    <Fragment>
+      <Button fullWidth onClick={handleOpenButtonClick}>
+        Open
+      </Button>
+      <Modal isOpen={isOpen} title="foo">
+        <div style={style}>
+          <Button onClick={handleCloseButtonClick} secondary>
+            Close
+          </Button>
+        </div>
+      </Modal>
+    </Fragment>
+  )
+}
+
+export const CloseButton = function () {
+  const [isOpen, setIsOpen] = useState(false)
+  function handleOpenButtonClick() {
+    setIsOpen(true)
+  }
+  function handleCloseButtonClick() {
+    setIsOpen(false)
+  }
+  const style = { height: '160px', padding: '12px', width: '240px' }
+  return (
+    <Fragment>
+      <Button fullWidth onClick={handleOpenButtonClick}>
+        Open
+      </Button>
+      <Modal
+        isOpen={isOpen}
+        onCloseButtonClick={handleCloseButtonClick}
+        secondary
+        title="foo"
+      >
+        <div style={style}>bar</div>
+      </Modal>
+    </Fragment>
+  )
+}
+
+export const CloseButtonPosition = function () {
+  const [isOpen, setIsOpen] = useState(false)
+  function handleOpenButtonClick() {
+    setIsOpen(true)
+  }
+  function handleCloseButtonClick() {
+    setIsOpen(false)
+  }
+  const style = { height: '160px', padding: '12px', width: '240px' }
+  return (
+    <Fragment>
+      <Button fullWidth onClick={handleOpenButtonClick}>
+        Open
+      </Button>
+      <Modal
+        closeButtonPosition="left"
+        isOpen={isOpen}
+        onCloseButtonClick={handleCloseButtonClick}
+        secondary
+        title="foo"
+      >
+        <div style={style}>bar</div>
+      </Modal>
+    </Fragment>
+  )
+}
+
+export const CloseButtonIcon = function () {
+  const [isOpen, setIsOpen] = useState(false)
+  function handleOpenButtonClick() {
+    setIsOpen(true)
+  }
+  function handleCloseButtonClick() {
+    setIsOpen(false)
+  }
+  const style = { height: '160px', padding: '12px', width: '240px' }
+  return (
+    <Fragment>
+      <Button fullWidth onClick={handleOpenButtonClick}>
+        Open
+      </Button>
+      <Modal
+        closeButtonIcon={<IconReturn32 />}
+        closeButtonPosition="left"
+        isOpen={isOpen}
+        onCloseButtonClick={handleCloseButtonClick}
+        secondary
+        title="foo"
+      >
+        <div style={style}>bar</div>
+      </Modal>
+    </Fragment>
+  )
+}
---
+++ packages/ui/src/components/modal/modal.tsx
@@ -0,0 +1,112 @@
+/** @jsx h */
+import { ComponentChild, Fragment, h, JSX, RefObject, render } from 'preact'
+import { useEffect, useRef } from 'preact/hooks'
+
+import { createClassName } from '../../utilities/create-class-name'
+import { IconCross32 } from '../icon/icon-32/icon-cross-32'
+import { Text } from '../text/text'
+import styles from './modal.css'
+
+export type ModalProps = {
+  children: ComponentChild
+  closeButtonIcon?: ComponentChild
+  closeButtonPosition?: ModalCloseButtonPosition
+  onOverlayClick?: JSX.MouseEventHandler<HTMLElement>
+  onCloseButtonClick?: JSX.MouseEventHandler<HTMLElement>
+  position?: ModalPosition
+  title?: string
+  isOpen: boolean
+}
+export type ModalCloseButtonPosition = 'left' | 'right'
+export type ModalPosition = 'bottom' | 'center' | 'left' | 'right' | 'top'
+
+export function Modal({
+  children,
+  closeButtonIcon = <IconCross32 />,
+  closeButtonPosition = 'right',
+  onCloseButtonClick,
+  onOverlayClick,
+  position = 'center',
+  title,
+  isOpen,
+  ...rest
+}: ModalProps): null {
+  const rootElementRef: RefObject<HTMLDivElement> = useRef(null)
+
+  useEffect(function () {
+    const rootElement = document.createElement('div')
+    document.body.appendChild(rootElement)
+    rootElementRef.current = rootElement
+    return function () {
+      document.body.removeChild(rootElement)
+    }
+  }, [])
+
+  useEffect(
+    function () {
+      if (rootElementRef.current === null) {
+        throw new Error('`rootElementRef.current` is `null`')
+      }
+      render(
+        <Fragment>
+          <div
+            {...rest}
+            className={createClassName([
+              styles.modal,
+              isOpen === true ? styles.visible : null,
+              styles[position]
+            ])}
+          >
+            {typeof onCloseButtonClick === 'undefined' &&
+            typeof title === 'undefined' ? null : (
+              <div className={styles.topBar}>
+                <div className={styles.title}>
+                  {typeof title === 'undefined' ? null : (
+                    <Text bold>{title}</Text>
+                  )}
+                </div>
+                {typeof onCloseButtonClick === 'undefined' ? null : (
+                  <button
+                    className={createClassName([
+                      styles.closeButton,
+                      closeButtonPosition === 'left'
+                        ? styles.closeButtonLeft
+                        : null
+                    ])}
+                    onClick={onCloseButtonClick}
+                  >
+                    <div className={styles.closeButtonBorder} />
+                    {closeButtonIcon}
+                  </button>
+                )}
+              </div>
+            )}
+            <div {...rest} className={styles.children}>
+              {children}
+            </div>
+          </div>
+          <div
+            class={styles.overlay}
+            onClick={
+              typeof onOverlayClick === 'undefined' ? undefined : onOverlayClick
+            }
+          />
+        </Fragment>,
+        rootElementRef.current
+      )
+    },
+    [
+      children,
+      closeButtonIcon,
+      closeButtonPosition,
+      isOpen,
+      onCloseButtonClick,
+      onOverlayClick,
+      position,
+      rest,
+      title
+    ]
+  )
+
+  return null
+}
