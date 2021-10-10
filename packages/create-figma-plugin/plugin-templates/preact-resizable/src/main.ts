5564011c09e15e9c588254080b34bccc69405854
Yuan Qing Lim <desk@yuanqing.sg>
2021-10-10 11:05:59 +0800

    Update plugin template

--- packages/create-figma-plugin/plugin-templates/preact-resizable/src/main.ts
+++ packages/create-figma-plugin/plugin-templates/preact-resizable/src/main.ts
@@ -5,12 +5,13 @@ import { ResizeWindowHandler } from './types'
 export default function () {
   on<ResizeWindowHandler>(
     'RESIZE_WINDOW',
-    function ({ width, height }: { width: number; height: number }) {
+    function (windowSize: { width: number; height: number }) {
+      const { width, height } = windowSize
       figma.ui.resize(width, height)
     }
   )
   showUI({
-    height: 240,
-    width: 240
+    width: 240,
+    height: 240
   })
 }
--- packages/create-figma-plugin/plugin-templates/preact-resizable/src/ui.tsx
+++ packages/create-figma-plugin/plugin-templates/preact-resizable/src/ui.tsx
@@ -4,18 +4,16 @@ import { emit } from '@create-figma-plugin/utilities'
 import { ResizeWindowHandler } from './types'
 
 function Plugin() {
-  useWindowResize(
-    function (windowSize: { width: number; height: number }) {
-      emit<ResizeWindowHandler>('RESIZE_WINDOW', windowSize)
-    },
-    {
-      maxHeight: 320,
-      maxWidth: 320,
-      minHeight: 120,
-      minWidth: 120,
-      resizeBehaviorOnDoubleClick: 'minimize'
-    }
-  )
+  function onWindowResize (windowSize: { width: number; height: number }) {
+    emit<ResizeWindowHandler>('RESIZE_WINDOW', windowSize)
+  }
+  useWindowResize(onWindowResize, {
+    minWidth: 120,
+    minHeight: 120,
+    maxWidth: 320,
+    maxHeight: 320,
+    resizeBehaviorOnDoubleClick: 'minimize'
+  })
   return null
 }
 
