03d1fed4190e0c14cfc8bab941a41d9975a625c8
Yuan Qing Lim <desk@yuanqing.sg>
2021-10-31 12:12:32 +0800

    Fix test

--- packages/build/test/build-async/build-async.ts
+++ packages/build/test/build-async/build-async.ts
@@ -591,7 +591,7 @@ async function installFigmaPluginTypingsAsync(): Promise<void> {
   await fs.ensureDir(join(process.cwd(), 'node_modules'))
   await new Promise<void>(function (resolve, reject) {
     exec(
-      `npm install --no-save @figma/plugin-typings@${constants.figmaPluginTypingsVersion}`,
+      `npm install --no-save @figma/plugin-typings@${constants.packageJson.versions.pluginTypings}`,
       {},
       function (error: ExecException | null): void {
         if (error) {
