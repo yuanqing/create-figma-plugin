23db307a2d060fe195549d8427048c703b9d87ec
Yuan Qing Lim <desk@yuanqing.sg>
2021-11-03 10:06:53 +0800

    Suppress `Cannot augment module console with value exports…` error

--- packages/build/src/utilities/type-check/filter-typescript-diagnostics.ts
+++ packages/build/src/utilities/type-check/filter-typescript-diagnostics.ts
@@ -9,7 +9,7 @@ export function filterTypeScriptDiagnostics(
     // We suppress this specific error when type-checking.
     // See https://github.com/figma/plugin-typings/pull/52/files#diff-7aa4473ede4abd9ec099e87fec67fd57afafaf39e05d493ab4533acc38547eb8
 
-    if (diagnostic.code !== 2451 || typeof diagnostic.file === 'undefined') {
+    if (typeof diagnostic.file === 'undefined') {
       return true
     }
     const fileName = diagnostic.file.fileName
@@ -20,9 +20,17 @@ export function filterTypeScriptDiagnostics(
     ) {
       return true
     }
+    const { code, messageText } = diagnostic
     if (
-      diagnostic.messageText !==
-      "Cannot redeclare block-scoped variable 'console'."
+      code === 2451 &&
+      messageText !== "Cannot redeclare block-scoped variable 'console'."
+    ) {
+      return true
+    }
+    if (
+      code === 2649 &&
+      messageText !==
+        "Cannot augment module 'console' with value exports because it resolves to a non-module entity."
     ) {
       return true
     }
