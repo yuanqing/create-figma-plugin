5f4239940b2b314dc0e077816b242ec6b209d11e
Yuan Qing Lim <desk@yuanqing.sg>
2022-05-25 06:00:50 -0700

    Edit Storybook config

--- packages/ui/.storybook/preview.js
+++ packages/ui/.storybook/preview.js
@@ -18,22 +18,39 @@ export const decorators = [
   }
 ]
 
-const groupOrder = ['components', 'inline-text', 'icons', 'layout', 'hooks']
+const groupOrder = ['layout', 'components', 'inline-text', 'icons', 'hooks']
+
+const componentIdRegex = new RegExp(`^(${groupOrder.join('|')})-(.+)`)
+
+function parseComponentId(componentId) {
+  const matches = componentId.match(componentIdRegex)
+  if (matches === null) {
+    return null
+  }
+  return {
+    groupName: matches[1],
+    title: matches[2]
+  }
+}
 
 export const parameters = {
   layout: 'centered',
   options: {
     storySort: function (x, y) {
+      // Same file
       if (x[1].componentId === y[1].componentId) {
-        // Same file
         return 0
       }
-      const xSplit = x[1].componentId.split(/-+/g).slice(0, 2)
-      const ySplit = y[1].componentId.split(/-+/g).slice(0, 2)
-      // Different top-level group
-      if (xSplit[0] !== ySplit[0]) {
-        const xGroupOrder = groupOrder.indexOf(xSplit[0])
-        const yGroupOrder = groupOrder.indexOf(ySplit[0])
+      const xx = parseComponentId(x[1].componentId)
+      const yy = parseComponentId(y[1].componentId)
+      // `componentId` doesn’t match the expected pattern
+      if (xx === null || yy === null) {
+        return x[1].id.localeCompare(y[1].id, undefined, { numeric: true })
+      }
+      // Different group name
+      if (xx.groupName !== yy.groupName) {
+        const xGroupOrder = groupOrder.indexOf(xx.groupName)
+        const yGroupOrder = groupOrder.indexOf(yy.groupName)
         if (xGroupOrder === -1) {
           return 1
         }
@@ -42,22 +59,22 @@ export const parameters = {
         }
         return xGroupOrder - yGroupOrder
       }
-      // Same component
-      if (xSplit[1] === ySplit[1]) {
-        if (
-          typeof x[1].parameters.order !== 'undefined' &&
-          typeof y[1].parameters.order !== 'undefined'
-        ) {
-          return x[1].parameters.order - y[1].parameters.order
-        }
-        if (typeof x[1].parameters.order !== 'undefined') {
-          return -1
-        }
-        if (typeof y[1].parameters.order !== 'undefined') {
-          return 1
-        }
+      // Both `order` defined
+      if (
+        typeof x[1].parameters.order !== 'undefined' &&
+        typeof y[1].parameters.order !== 'undefined'
+      ) {
+        return x[1].parameters.order - y[1].parameters.order
       }
-      return x[1].id.localeCompare(y[1].id, undefined, { numeric: true })
+      // Either `order` defined
+      if (typeof x[1].parameters.order !== 'undefined') {
+        return -1
+      }
+      if (typeof y[1].parameters.order !== 'undefined') {
+        return 1
+      }
+      // Both `order` undefined
+      return xx.title.localeCompare(yy.title, undefined, { numeric: true })
     }
   },
   themes: {
