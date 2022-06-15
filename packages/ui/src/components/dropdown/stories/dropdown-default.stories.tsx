ad7888ed5435a5c7ecafcdd21bc70d6cb9cbc2c9
Yuan Qing Lim <desk@yuanqing.sg>
2022-06-15 20:38:06 +0800

    Revert

--- packages/ui/src/components/dropdown/stories/dropdown-default.stories.tsx
+++ packages/ui/src/components/dropdown/stories/dropdown-default.stories.tsx
@@ -25,22 +25,7 @@ export const Empty = function () {
     { value: 'baz' },
     { separator: true },
     { header: 'Header' },
-    { value: 'qux' },
-    { value: '1' },
-    { value: '2' },
-    { value: '3' },
-    { value: '4' },
-    { value: '5' },
-    { value: '6' },
-    { value: '7' },
-    { value: '8' },
-    { value: '9' },
-    { value: '10' },
-    { value: '11' },
-    { value: '12' },
-    { value: '13' },
-    { value: '14' },
-    { value: '15' }
+    { value: 'qux' }
   ]
   function handleChange(event: JSX.TargetedEvent<HTMLInputElement>) {
     const newValue = event.currentTarget.value
