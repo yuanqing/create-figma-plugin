c3d5fd56a6de193cfdad5d00dc4a1485684f3365
Yuan Qing Lim <desk@yuanqing.sg>
2023-08-16 23:00:42 +0800

    Refine name

--- packages/create-figma-plugin/src/utilities/create-name.ts
+++ packages/create-figma-plugin/src/utilities/create-name.ts
@@ -2,6 +2,9 @@ import { titleCase } from 'title-case'
 
 export function createName(directoryName: string): string {
   return titleCase(
-    directoryName.replace(/^fig(?:ma|jam)-/, '').replace(/-/g, ' ')
+    directoryName
+      .replace(/^fig(?:ma|jam)-/, '')
+      .replace(/tailwindcss/, 'Tailwind CSS')
+      .replace(/-/g, ' ')
   )
 }
