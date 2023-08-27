5c96b4771415dbae38a6d766a85ca7ed4979780f
Yuan Qing Lim <desk@yuanqing.sg>
2023-08-27 17:03:23 +0800

    Revert changes made to `esbuild-css-modules-plugin.ts`

--- packages/build/src/utilities/build-bundles-async/esbuild-css-modules-plugin.ts
+++ packages/build/src/utilities/build-bundles-async/esbuild-css-modules-plugin.ts
@@ -78,7 +78,6 @@ async function createCssFilePathAsync(
 }
 
 const backQuoteRegex = /`/g
-const colonRegex = /:/g
 
 async function createGlobalCssJavaScriptAsync(
   cssFilePath: string,
@@ -105,9 +104,7 @@ async function createGlobalCssJavaScriptAsync(
     if (document.getElementById('${elementId}') === null) {
       const element = document.createElement('style');
       element.id = '${elementId}';
-      element.textContent = \`${css
-        .replace(backQuoteRegex, '\\`')
-        .replace(colonRegex, '\\:')}\`;
+      element.textContent = \`${css.replace(backQuoteRegex, '\\`')}\`;
       document.head.${isBaseCss === true ? 'prepend' : 'append'}(element);
     }
     export default {};
@@ -152,9 +149,7 @@ async function createCssModulesJavaScriptAsync(
     if (document.getElementById('${elementId}') === null) {
       const element = document.createElement('style');
       element.id = '${elementId}';
-      element.textContent = \`${css
-        .replace(backQuoteRegex, '\\`')
-        .replace(colonRegex, '\\:')}\`;
+      element.textContent = \`${css.replace(backQuoteRegex, '\\`')}\`;
       document.head.append(element);
     }
     export default ${classNamesJson};
