64316cdbb94d4d585281752ba2b18b539538c9a1
Arya <aryayadav3497@gmail.com>
2024-01-03 18:18:18 +0530

    fix: added missing packages

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,23 +1,23 @@
-import { defineConfig } from 'vite'
-import dts from 'vite-plugin-dts'
-import path from 'path'
-import pkjJSON from './package.json'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+import path from "path";
+import pkjJSON from "./package.json";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   build: {
     lib: {
-      entry: path.resolve(__dirname, './src/index.ts'),
-      formats: ['es', 'cjs'],
-      fileName: 'index'
+      entry: path.resolve(__dirname, "./src/index.ts"),
+      formats: ["es", "cjs"],
+      fileName: "index",
     },
     emptyOutDir: true,
     rollupOptions: {
-      external: Object.keys(pkjJSON.peerDependencies)
-    }
+      external: Object.keys(pkjJSON.peerDependencies),
+    },
   },
   css: {
-    modules: {}
+    modules: {},
   },
-  plugins: [react(), dts()]
-})
+  plugins: [react(), dts()],
+});
