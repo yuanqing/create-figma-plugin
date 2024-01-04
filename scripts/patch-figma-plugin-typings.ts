3af852062634565966c5d3dc6d199ab20b34bfb8
Arya Yadav <aryayadav3497@gmail.com>
2024-01-04 12:28:21 +0530

    feat: removed the extended path and moved writeFileAsync

--- scripts/patch-figma-plugin-typings.ts
+++ scripts/patch-figma-plugin-typings.ts
@@ -1,34 +1,44 @@
-import fs from 'node:fs/promises'
-import { dirname, resolve } from 'node:path'
-import { fileURLToPath } from 'node:url'
+import fs from "node:fs/promises";
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+import { pathExists } from "path-exists";
 
-import { writeFileAsync } from '../packages/common/src/write-file-async.js'
+export async function writeFileAsync(
+  outputFilePath: string,
+  fileContents: string | Buffer
+): Promise<void> {
+  const directoryPath = dirname(outputFilePath);
+  if ((await pathExists(directoryPath)) === false) {
+    await fs.mkdir(directoryPath, { recursive: true });
+  }
+  await fs.writeFile(outputFilePath, fileContents);
+}
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const strings = [
-  'const console: Console\n',
-  'const fetch: (url: string, init?: FetchOptions) => Promise<FetchResponse>\n'
-]
+  "const console: Console\n",
+  "const fetch: (url: string, init?: FetchOptions) => Promise<FetchResponse>\n",
+];
 
 async function main(): Promise<void> {
   try {
     const tsFilePath = resolve(
       __dirname,
-      '..',
-      'node_modules',
-      '@figma',
-      'plugin-typings',
-      'index.d.ts'
-    )
-    let result = await fs.readFile(tsFilePath, 'utf8')
+      "..",
+      "node_modules",
+      "@figma",
+      "plugin-typings",
+      "index.d.ts"
+    );
+    let result = await fs.readFile(tsFilePath, "utf8");
     for (const string of strings) {
-      result = result.replace(string, '')
+      result = result.replace(string, "");
     }
-    await writeFileAsync(tsFilePath, result)
+    await writeFileAsync(tsFilePath, result);
   } catch (error: any) {
-    console.error(error.message) // eslint-disable-line no-console
-    process.exit(1)
+    console.error(error.message); // eslint-disable-line no-console
+    process.exit(1);
   }
 }
-main()
+main();
