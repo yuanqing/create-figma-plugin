f3bfba89b0ee667c85c3b95213fd8b456a13c77a
Yuan Qing Lim <desk@yuanqing.sg>
2024-10-14 05:29:46 +0800

    Migrate `SelectableItem`

--- packages/ui/src/components/selectable-item/selectable-item.tsx
+++ packages/ui/src/components/selectable-item/selectable-item.tsx
@@ -1,7 +1,7 @@
 import { ComponentChildren, h } from 'preact'
 import { useCallback } from 'preact/hooks'
 
-import { IconMenuCheckmarkChecked16 } from '../../icons/icon-16/icon-menu-checkmark-checked-16.js'
+import { IconUi3CheckboxCheck16 } from '../../icons/icon-16/icon-ui-3-checkbox-check-16.js'
 import { Event, EventHandler } from '../../types/event-handler.js'
 import { FocusableComponentProps } from '../../types/focusable-component-props.js'
 import { createClassName } from '../../utilities/create-class-name.js'
@@ -80,13 +80,14 @@ export const SelectableItem = createComponent<
         tabIndex={0}
         type="checkbox"
       />
-      <div class={styles.box} />
-      <div class={styles.children}>{children}</div>
-      {value === true ? (
-        <div class={styles.icon}>
-          <IconMenuCheckmarkChecked16 />
-        </div>
-      ) : null}
+      <div class={styles.box}>
+        <div class={styles.children}>{children}</div>
+        {value === true ? (
+          <div class={styles.icon}>
+            <IconUi3CheckboxCheck16 />
+          </div>
+        ) : null}
+      </div>
     </label>
   )
 })
